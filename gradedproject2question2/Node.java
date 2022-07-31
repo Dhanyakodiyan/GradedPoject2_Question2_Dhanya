4893ee24867690592ed2ccc020bce7156c4129c0
Dhanyakodiyan <106870488+Dhanyakodiyan@users.noreply.github.com>
2022-07-31 21:24:21 +0530

    Add files via upload

---
+++ gradedproject2question2/Node.java
@@ -0,0 +1,16 @@
+package gradedproject2question2;
+
+public class  Node {
+	
+	 int value;
+	Node left;
+	Node right;
+	
+
+	public Node(int item) 
+	{
+	value = item;
+	left = right = null;
+	}
+
+}
